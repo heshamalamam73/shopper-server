ead92c0eebe4795c27b1a44a6e82249e65c6c631
mahmoud hassan <heshamalamam73@gmail.com>
2021-08-17 09:42:16 +0200

    fix(general): fix accepted client order table style

---
+++ commitlint.config.js
@@ -0,0 +1,55 @@
+// var prefixes = ['api'];
+var scopes = [
+  "general",
+  "root-config",
+  "models",
+  "tests",
+  "database",
+  "api",
+  "config",
+  "login",
+  "rma",
+  "menu",
+  "bug",
+  "profile",
+  "dashboard",
+];
+
+// var _prefixed = scopes.map(function (s) {
+//     return prefixes.map(function (p) {
+//         return p + '-' + s;
+//     });
+// });
+var prefixed = [].concat.apply([], scopes);
+var config = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [
+      2,
+      "always",
+      [
+        // special:
+        "build",
+        "ci",
+        "release",
+
+        // common-dev
+        "chore",
+        "wip",
+        "feat",
+        "fix",
+        "improve",
+        "refactor",
+        "lint",
+
+        //
+        "test",
+        "docs",
+      ],
+    ],
+    "scope-enum": [2, "always", prefixed],
+  },
+};
+
+module.exports = config;
